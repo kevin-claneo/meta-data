602c1f0c7f2b2eadd6b020d7e803bf1b08586301
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-04-04 17:28:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def main():
             client, model = handle_api_keys()
 
             # Choose a language for the Meta Data
-            language = st.selectbox("Choose a language 🇺🇳 for the Meta Data:", LANGUAGES)
+            language = st.selectbox("Choose a language🌐 for the Meta Data:", LANGUAGES)
 
             # Generate Meta Data
             if st.button("Generate Meta Data"):
